35412ce1fe8c8474192c8c4055d674565ad9ce5e
bagdat <150107026@stu.sdu.edu.kz>
2019-05-11 19:28:46 +0600

    user

--- src/main/java/model/User/model/User.java
+++ src/main/java/model/User/model/User.java
@@ -1,9 +1,51 @@
 package model.User.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "user")
 public class User {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Size(max = 45, message = "Название слишком длинное")
+    @NotNull(message = "Логин не может быть пустым")
+    @Column(name = "login")
+    private String login;
+
+    @Size(max = 45, message = "Пароль слишком длинный")
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @NotNull
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(@NotNull String login) {
+        this.login = login;
+    }
+
+    @NotNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotNull String password) {
+        this.password = password;
+    }
 }
