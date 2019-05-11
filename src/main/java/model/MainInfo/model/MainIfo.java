944daf8f2ce6dd510a2aebb5890bc2b4e9a03a1e
bagdat <150107026@stu.sdu.edu.kz>
2019-05-11 19:36:38 +0600

    main info

--- src/main/java/model/MainInfo/model/MainIfo.java
+++ src/main/java/model/MainInfo/model/MainIfo.java
@@ -1,8 +1,83 @@
 package model.MainInfo.model;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import model.File.model.File;
+import model.User.model.User;
+
 import javax.persistence.*;
+import javax.sql.DataSource;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.DefaultValue;
+import java.util.Date;
 
 @Entity
 @Table(name = "main_info")
 public class MainIfo {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "file_id", insertable = false, updatable = false)
+    private File file;
+    
+    @NotNull
+    @Column(name = "upload_date")
+    private Date uploadDate;
+    
+    @NotNull
+    @Column(name = "status")
+    private Boolean status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @NotNull
+    public Date getUploadDate() {
+        return uploadDate;
+    }
+
+    
+    public void setUploadDate(@NotNull Date uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    @NotNull
+    public Boolean getStatus() {
+        return status;
+    }
+
+    
+    public void setStatus(@NotNull Boolean status) {
+        this.status = status;
+    }
 }
